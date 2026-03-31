df4437704622b9c8ccd1dc60833d79197720f360
devendrachoudharycodes <devendra.choudhary.codes@gmail.com>
2026-03-31 20:19:14 +0530

    set up hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -42,6 +44,18 @@ android {
 }
 
 dependencies {
+    //hilt
+    implementation(libs.hilt.android)
+    //hilt compiler
+    ksp(libs.hilt.compiler)
+
+
+
+
+
+
+
+    //android studio dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/genzsage/genzsageandroidapp/MyApp.kt
@@ -0,0 +1,8 @@
+package com.genzsage.genzsageandroidapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class MyApp: Application()
